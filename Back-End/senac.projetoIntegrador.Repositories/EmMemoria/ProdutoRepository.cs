696dd7be19891195672a7648e2a784afe984c833
Sandro Marra <sandromarra@gmail.com>
2023-11-12 20:14:14 -0300

    Cadastros de produtos

--- Back-End/senac.projetoIntegrador.Repositories/EmMemoria/ProdutoRepository.cs
+++ Back-End/senac.projetoIntegrador.Repositories/EmMemoria/ProdutoRepository.cs
@@ -10,11 +10,44 @@ namespace senac.projetoIntegrador.Repositories.EmMemoria
             new Produto
             {
                 Id = 1,
-                Nome = "Hambúrguer Clássico",
-                Descricao = "Um hambúrguer clássico com carne grelhada, alface, tomate e queijo cheddar.",
+                Nome = "Classic Burguer",
+                Descricao = "Um hambúrguer de carne grelhada, alface, tomate e queijo cheddar.",
                 Preco = 9.99M,
-                CaminhoImagem = "/imagens/hamburguer1.jpg"
+                CaminhoImagem = "/src/assets/burger5.png"
             }
+
+            {
+                Id = 2,
+                Nome = "Cheesebuguer",
+                Descricao = "Hambúrguer de carne, queijo, alface, tomate e maionese em um pão de hambúrguer.",
+                Preco = 8.99M,
+                CaminhoImagem = "/src/assets/burger1.png"
+            }
+
+            {
+                Id = 3,
+                Nome = "Bacon Burguer",
+                Descricao = "Hambúrguer de carne coberto com bacon crocante, queijo, alface, tomate e maionese, servido em um pão de hambúrguer.",
+                Preco = 9.99M,
+                CaminhoImagem = "/src/assets/burger2.png"
+            }
+
+            {
+                Id = 3,
+                Nome = "Veggie Burguer",
+                Descricao = "Hambúrguer vegetariano à base de vegetais, acompanhado de queijo, alface, tomate e maionese em um pão de hambúrguer.",
+                Preco = 6.99M,
+                CaminhoImagem = "/src/assets/burger6.png"
+            }
+
+            {
+                Id = 3,
+                Nome = "Double Cheeseburger",
+                Descricao = "Dois hambúrgueres de carne, dupla porção de queijo, alface, tomate e maionese, servidos em um pão de hambúrguer.",
+                Preco = 10.99M,
+                CaminhoImagem = "/src/assets/burger4.png"
+            }
+
         };
 
         public List<Produto> List()
