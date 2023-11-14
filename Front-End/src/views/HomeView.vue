3778cbb5daa8156d5637cf4dce4196828ad740af
ruanwillians <ruanwillians.mn@gmail.com>
2023-11-13 21:29:16 -0300

    Implements conections for APIS

--- Front-End/src/views/HomeView.vue
+++ Front-End/src/views/HomeView.vue
@@ -1,11 +1,16 @@
 <template>
   <Banner />
   <div class="container">
-  <h1 id='title' >OFERTAS</h1>
+    <h1 id="title">OFERTAS</h1>
     <div class="burgers">
-    <span  v-for="burger in hamburgers">
-       <Card :name="burger.name" :price="burger.price" :image="burger.img" />
-    </span>
+      <Card
+        v-for="burger in hamburgers"
+        :id="burger.id"
+        :nome="burger.nome"
+        :preco="burger.preco"
+        :caminhoImagem="burger.caminhoImagem"
+        :descricao="burger.descricao"
+      />
     </div>
   </div>
 </template>
@@ -13,15 +18,37 @@
 <script setup>
 import Banner from "../components/Banner.vue";
 import Card from "../components/Card.vue";
+import { BASEURL } from "../../env";
+import { onBeforeMount, ref } from "vue";
 
-const hamburgers = [
-  { name: "Classic Burger", price: 7.99, img:"/src/assets/burger5.png" },
-  { name: "Cheeseburger", price: 8.99, img:"/src/assets/burger1.png" },
-  { name: "Bacon Burger", price: 9.99, img:"/src/assets/burger2.png"  },
-  { name: "Veggie Burger", price: 6.99, img:"/src/assets/burger6.png"  },
-  { name: "Double Cheeseburger", price: 10.99, img:"/src/assets/burger4.png"  },
-];
+const hamburgers = ref([]);
 
+onBeforeMount(async () => {
+  hamburgers.value = await getBurgers();
+});
+
+async function getBurgers() {
+  try {
+    const response = await fetch(`${BASEURL}/api/Produto`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: localStorage.getItem("token"),
+      },
+    });
+
+    if (response.ok) {
+      const hamburgersData = await response.json();
+      return hamburgersData;
+    } else {
+      console.error("Erro na solicitação GET. Código de status:", response.status);
+      return [];
+    }
+  } catch (error) {
+    console.error("Erro na solicitação GET:", error);
+    return [];
+  }
+}
 </script>
 
 <style>
@@ -29,12 +56,12 @@ const hamburgers = [
   margin-top: -1em;
   display: flex;
   flex-wrap: wrap;
-  justify-content: space-between; 
+  justify-content: space-between;
 }
 
 #title {
-  -webkit-text-stroke: 2px #9758A6; /* Define a largura e a cor do contorno */
-  color: transparent; /* Torna o preenchimento do texto transparente */
+  -webkit-text-stroke: 2px #9758a6;
+  color: transparent;
 }
 
 .container {
@@ -42,13 +69,12 @@ const hamburgers = [
   display: flex;
   flex-direction: column;
   justify-content: center;
-  align-items: center
+  align-items: center;
 }
 
 @media (max-width: 750px) {
   .burgers {
-    justify-content: center
+    justify-content: center;
   }
 }
-
 </style>
